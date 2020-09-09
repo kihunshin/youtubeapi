3faa0579b331190427fae7d69e4eb5927729beed
unknown <kihunshin>
2020-09-09 19:54:57 +0900

    first commit

---
+++ src/components/Searchbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export class Searchbar extends React.Component {
+    handleChange = (event) => {
+        this.setState({
+            term: event.target.value
+        })
+    }
+    handleSubmit = event => {
+        event.preventDefault();
+        this.props.handleFormSubmit(this.state.term);
+    }
+
+    render() {
+        return (
+            <h2 style={{textAlign:"center"}}><img style={{width:'200px', height:'100px' , justifyContent:'center'}} src='http://www.thatitguy.net/wp-content/uploads/2018/08/1280px-Logo_of_YouTube_2015-2017.svg.png' alt="youtube logo"/></h2>
+                <div className='search-bar ui segment'>
+                    <form onSubmit={this.handleSubmit} className='ui form'>
+                        <div className='field'>
+                            <label htmlFor="video-search">Video Search</label>
+                            <input onChange={this.handleChange} name='video-search' type="text" placeholder="검색"/>
+                        </div>
+                    </form>
+                </div>
+        )    
+        
+    }
+}
---
+++ src/components/VideoDetail.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const VideoDetail = ({ video }) => {
+    if(!video) {
+        return <div>
+            <h1>키워드를 입력하세요</h1>
+            <br></br>
+            <p style= {{fontSize='25px'}}>
+유튜브 Api
+
+            </p>
+        </div>
+    }
+
+    const videoSrc = 'http://www.youtube.com/embed/${video.id.videoId}';
+    console.log(typeof video);
+    return (
+        <div>
+            <div className="uo embed">
+                <iframe src={videoSrc} allowFullScreen title="Video player"/>
+            </div>
+            <div className="ui segment">
+                <h4 className="ui header">{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
+        </div>
+    )
+}
